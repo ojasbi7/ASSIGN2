c74b58ddaf335220c541674c750a0fccd505d326
ojasbi7 <70551107+ojasbi7@users.noreply.github.com>
2020-10-09 12:59:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,39 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+document.addEventListener('DOMContentLoaded', function(){
 
+  document.querySelector("#new-task").onsubmit= function(){
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+    const li=document.createElement('li');
 
+    let task_text= document.querySelector("#task").value;
+    let new_task_html = `
+                            <span> ${task_text} </span>
+                            <button class= "complete"> Task is Complete </button>
+                            <button class="remove"> Remove task </button>
+                        `
+    li.innerHTML= new_task_html;
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+      li.style.color= document.querySelector("#priority-change").value;
+    var arr= [];
+    arr.push(li);
+    document.querySelector("#task_list").append(li);
+    document.querySelector("#task").value= '';
+    return false;
   }
-}, false);
 
-
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var inputValue1 = document.getElementById("myInput1").value;
-  var t = document.createTextNode(inputValue);
-     var s = document.createTextNode(inputValue1);
-  li.appendChild(t);
-  
-  if (inputValue1== 1){
- li.style.backgroundColor = "gray";
- 
- 
-  }
-  else if (inputValue1==2){
-   li.style.backgroundColor = "pink";
-    
-  }
-  else if (inputValue1==3){
-     li.style.backgroundColor = "babyblue";
-  
-  }
- if(inputValue1 <1){
- 
-  alert("Only numbers between 1-3!!");
-  }
-   if(inputValue1 >3){
- 
-  alert("Only numbers between 1-3!!");
-  }
-  if (inputValue === '') {
-    alert("Requires Text!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+  document.addEventListener('click', function(){
+    element= event.target;
+    if(element.className=== 'remove'){
+      element.parentElement.remove();
+      arr.remove(element.parentElement);
     }
-  }
-}
+
+  })
+
+  document.addEventListener('click', function(){
+    element=event.target;
+    if(element.className==='complete'){
+      element.parentElement.style.textDecoration= "black line-through";
+      element.remove();
+    }
+  })
+})
